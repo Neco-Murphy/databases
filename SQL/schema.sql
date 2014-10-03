56a47d7f033e83e922c3ac0f68a1c56530bc4f68
Neco-Murphy <stogamori@gmail.com>
2014-10-03 12:30:03 -0700

    Implemented schema.sql

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,9 +1,14 @@
-CREATE DATABASE chat;
+/*CREATE DATABASE chat;*/
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageId int,
+  username varchar(20),
+  time timestamp,
+  room varchar(20),
+  text text
 );
 
 /* Create other tables and define schemas for them here! */
